0170491dcf12ab0b8182ee93ed2834e359f5e1b7
heesok2 <heesok2@gmail.com>
2019-04-09 12:17:31 +0900

    * OpenGL Sample 파일

--- Skeleton/src/OpenGL/main.cpp
+++ Skeleton/src/OpenGL/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <glut.h>
 
+void MyDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POLYGON);
+	{
+		glVertex3f(-0.5, -0.5, 0.0);
+		glVertex3f(0.5, -0.5, 0.0);
+		glVertex3f(0.5, 0.5, 0.0);
+		glVertex3f(-0.5, 0.5, 0.0);
+	}
+	glEnd();
+	glFlush();
+}
+
 int main(void)
 {
-	std::cout << "Sample Project\n";
-	std::cout << "Hello World!\n";
-
-	system("pause");
-
+	glutCreateWindow("OpenGL Drawing Example");
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
 	return 0;
 }
