3f16ac99f76b433f4bf8f9b155c462b98d3d6e24
creatorsok2 <creatorsok2@gmail.com>
2019-11-25 11:24:29 +0900

    * File Ctrl 수정

--- WBF/src/WBF_LIB/FileCtrl.h
+++ WBF/src/WBF_LIB/FileCtrl.h
@@ -6,11 +6,19 @@ class __MY_EXT_CLASS__ CFileCtrl
 {
 public:
 	CFileCtrl();
+	CFileCtrl(const CString& strFilePath);
 	virtual ~CFileCtrl();
 
 public:
-	static BOOL Exist(const CString csFilePath);
+	void SetFilePath(const CString& strFilePath);
+	BOOL Exist();
+
+public:
+	static BOOL Exist(const CString& strFullPath);
 	static CString Absolute2Relative(const CString& src, const CString& trg);
+
+protected:
+	CString m_strFullPath;
 
 };
 
