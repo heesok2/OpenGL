ab287a5dbaa716781c518ddd5975d38dacf9704a
heesok2 <heesok2@gmail.com>
2019-11-27 16:58:25 +0900

    * 중복코드 제거

--- WBF/src/WBF_LIB/StringConverter.cpp
+++ WBF/src/WBF_LIB/StringConverter.cpp
@@ -16,64 +16,14 @@ CStringConverter::~CStringConverter()
 {
 }
 
-void CStringConverter::CSTR2STR(IN const CString & cstr, OUT std::string & str)
-{
-	str = CSTR2STR(cstr);
-}
-
 void CStringConverter::CSTR2WSTR(IN const CString & cstr, OUT std::wstring wstr)
 {
 	wstr = CSTR2WSTR(cstr);
 }
 
-void CStringConverter::STR2CSTR(IN const std::string & str, OUT CString & cstr)
-{
-	cstr = STR2CSTR(str);
-}
-
-void CStringConverter::STR2WSTR(IN const std::string & str, OUT std::wstring wstr)
-{
-	wstr = STR2WSTR(str);
-}
-
 void CStringConverter::WSTR2CSTR(IN const std::wstring & wstr, OUT CString & cstr)
 {
 	cstr = WSTR2CSTR(wstr);
-}
-
-void CStringConverter::WSTR2STR(IN const std::wstring wstr, OUT std::string & str)
-{
-	str = WSTR2STR(wstr);
-}
-
-std::string CStringConverter::CSTR2STR(IN const CString & cstr)
-{
-	return std::string(CT2CA(cstr.operator LPCWSTR()));
-}
-
-std::wstring CStringConverter::CSTR2WSTR(IN const CString & cstr)
-{
-	return std::wstring(cstr.operator LPCWSTR());
-}
-
-CString CStringConverter::STR2CSTR(IN const std::string & str)
-{
-	return CString::CStringT(CA2CT(str.c_str()));
-}
-
-std::wstring CStringConverter::STR2WSTR(IN const std::string & str)
-{
-	return std::wstring(CA2CT(str.c_str()));
-}
-
-CString CStringConverter::WSTR2CSTR(IN const std::wstring & wstr)
-{
-	return CString::CStringT(wstr.c_str());
-}
-
-std::string CStringConverter::WSTR2STR(IN const std::wstring wstr)
-{
-	return std::string(CT2CA(wstr.c_str()));
 }
 
 void CStringConverter::CSTR2STR(IN const CString & cstr, OUT std::string & str, UINT CodePage)
@@ -94,6 +44,16 @@ void CStringConverter::STR2WSTR(IN const std::string & str, OUT std::wstring wst
 void CStringConverter::WSTR2STR(IN const std::wstring wstr, OUT std::string & str, UINT CodePage)
 {
 	str = WSTR2STR(wstr, CodePage);
+}
+
+std::wstring CStringConverter::CSTR2WSTR(IN const CString & cstr)
+{
+	return std::wstring(cstr.operator LPCWSTR());
+}
+
+CString CStringConverter::WSTR2CSTR(IN const std::wstring & wstr)
+{
+	return CString::CStringT(wstr.c_str());
 }
 
 std::string CStringConverter::CSTR2STR(IN const CString & cstr, UINT CodePage)
--- WBF/src/WBF_LIB/StringConverter.h
+++ WBF/src/WBF_LIB/StringConverter.h
@@ -14,29 +14,19 @@ public:
 	virtual ~CStringConverter();
 
 public:
-	static void CSTR2STR(IN const CString& cstr, OUT std::string& str);
 	static void CSTR2WSTR(IN const CString& cstr, OUT std::wstring wstr);
-	static void STR2CSTR(IN const std::string& str, OUT CString& cstr);
-	static void STR2WSTR(IN const std::string& str, OUT std::wstring wstr);
 	static void WSTR2CSTR(IN const std::wstring& wstr, OUT CString& cstr);
-	static void WSTR2STR(IN const std::wstring wstr, OUT std::string& str);
-	
-	static std::string CSTR2STR(IN const CString& cstr);
+	static void CSTR2STR(IN const CString& cstr, OUT std::string& str, UINT CodePage = CP_ACP);
+	static void STR2CSTR(IN const std::string& str, OUT CString& cstr, UINT CodePage = CP_ACP);
+	static void STR2WSTR(IN const std::string& str, OUT std::wstring wstr, UINT CodePage = CP_ACP);
+	static void WSTR2STR(IN const std::wstring wstr, OUT std::string& str, UINT CodePage = CP_ACP);
+
 	static std::wstring CSTR2WSTR(IN const CString& cstr);
-	static CString STR2CSTR(IN const std::string& str);
-	static std::wstring STR2WSTR(IN const std::string& str);
 	static CString WSTR2CSTR(IN const std::wstring& wstr);
-	static std::string WSTR2STR(IN const std::wstring wstr);
-
-	static void CSTR2STR(IN const CString& cstr, OUT std::string& str, UINT CodePage);
-	static void STR2CSTR(IN const std::string& str, OUT CString& cstr, UINT CodePage);
-	static void STR2WSTR(IN const std::string& str, OUT std::wstring wstr, UINT CodePage);
-	static void WSTR2STR(IN const std::wstring wstr, OUT std::string& str, UINT CodePage);
-
-	static std::string CSTR2STR(IN const CString& cstr, UINT CodePage);
-	static CString STR2CSTR(IN const std::string& str, UINT CodePage);
-	static std::wstring STR2WSTR(IN const std::string& str, UINT CodePage);
-	static std::string WSTR2STR(IN const std::wstring wstr, UINT CodePage);
+	static std::string CSTR2STR(IN const CString& cstr, UINT CodePage = CP_ACP);
+	static CString STR2CSTR(IN const std::string& str, UINT CodePage = CP_ACP);
+	static std::wstring STR2WSTR(IN const std::string& str, UINT CodePage = CP_ACP);
+	static std::string WSTR2STR(IN const std::wstring wstr, UINT CodePage = CP_ACP);
 };
 
 #include "HeaderPost.h"
